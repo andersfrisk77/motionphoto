a69ed64e3b650e23f82a33df52919c9fce332420
Anders Frisk <andersfrisk77@gmail.com>
2018-01-05 23:14:03 +0100

    Clenaup

--- MotionPhoto/Picasa.cs
+++ MotionPhoto/Picasa.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
